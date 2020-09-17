92c2236e3bc7908a4b93f69f12ec0f944d2315e1
manikdahiphale2016 <manik.dahiphale2016@gmail.com>
2020-09-17 16:15:39 +0530

    first commit

---
+++ ppmtool/src/test/java/com/manikit/ppmtool/PpmtoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manikit.ppmtool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PpmtoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
