efbe4d3cf80a98b4e2fc320c0b53634028ed62de
manikdahiphale2016 <manik.dahiphale2016@gmail.com>
2020-09-18 10:00:32 +0530

    ProjectRepository created

---
+++ ppmtool/src/main/java/com/manikit/ppmtool/repositories/ProjectRepository.java
@@ -0,0 +1,15 @@
+package com.manikit.ppmtool.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.manikit.ppmtool.demo.Project;
+
+@Repository
+public interface ProjectRepository extends CrudRepository<Project, Long> {
+
+	@Override
+	Iterable<Project> findAllById(Iterable<Long> ids);
+	
+	
+}
